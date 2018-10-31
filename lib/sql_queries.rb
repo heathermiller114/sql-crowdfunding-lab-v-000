28e9b8a16c487f67379bd88c63349d4f3fb12629
Heather Miller <heather.miller114@gmail.com>
2018-10-31 21:59:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 "SELECT user.name, user.age, pledges.amount
 FROM users
 INNER JOIN pledges
-on user.id = pledges.user_id"
+ON user.id = pledges.user_id
+GROUP BY "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
